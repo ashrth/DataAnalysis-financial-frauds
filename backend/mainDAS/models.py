9945a1d32210386b8fedaa4f632e745aa6e1dc17
ks27082002 <119859686+ks27082002@users.noreply.github.com>
2023-12-19 12:59:39 +0530

    Update models.py

--- backend/mainDAS/models.py
+++ backend/mainDAS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class FraudAccount(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    accountId = models.CharField(max_length=10, unique=True)
+    mobileNo = models.CharField(max_length=10)
+    fraud_summary = models.TextField()
+    flagged = models.BooleanField(default=False)
+
 # Create your models here.
