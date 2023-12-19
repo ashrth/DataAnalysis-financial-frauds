ff464908f90b4862cc76c7dfbd124d031bb13cbb
ashrth <aashrrita@gmail.com>
2023-12-19 12:36:35 +0530

    user model created

--- backend/authDAS/models.py
+++ backend/authDAS/models.py
@@ -2,14 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    first_name= models.TextField()
-    last_name=models.TextField()
-    email=models.EmailField()
-    password= models.TextField()
+    first_name= models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True, max_length=30)
+    password= models.CharField(max_length=70)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
+    
 
 
 
