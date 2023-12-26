9d47a88c564ee0b1babb9082031c6073bf7410c3
Yashgautam123 <gautamyashwant932@gmail.com>
2023-12-26 08:21:44 +0530

    added barchart sidebar topbar progress circle statbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,36 @@
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import AuthProtection from './components/auth-protection';
-// import Home from "./pages/Home";
-// import Tables from "./pages/Tables";
-// import Billing from "./pages/Billing";
-// import Rtl from "./pages/Rtl";
-// import Profile from "./pages/Profile";
-// import SignUp from "./pages/SignUp";
-// import SignIn from "./pages/SignIn";
-// import Main from "./components/layout/Main";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Bar from "./scenes/bar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "../src/styles/theme";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <Routes>
-      <Route element={
-        <AuthProtection>
-          <Outlet />
-        </AuthProtection>
-      }>
-        <Route path="/sign-up" element={<h1>sign-up</h1>} />
-        <Route path="/sign-in" element={<h1>in</h1>} />
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<h1>dashboard</h1>} />
-        <Route path="/tables" element={<h1>tables</h1>} />
-        <Route path="/billing" element={<h1>billing</h1>} />
-        <Route path="/profile" element={<h1>profile</h1>} />
-        <Route path="/chat" element={<h1>chat</h1>} />
-        <Route path="/track-account/:id" element={<h1>track-account</h1>} />
-      </Route>
-    </Routes>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ConfigProvider} from 'antd'
 import {AuthProvider} from './hooks/useAuth.js';
-import { ANTD_THEME } from './styles/theme.js'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './main.css'
@@ -10,11 +9,7 @@ import './main.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ConfigProvider theme={ANTD_THEME}>
-        <AuthProvider>
           <App />
-        </AuthProvider>
-      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
